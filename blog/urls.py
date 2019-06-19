ac2be37e3a765a466fbd8085237c638dba81de4e
Dewok Satria <windowsdewa@gmail.com>
2019-06-20 01:18:44 +0700

    Add a login autentication, & add some styling css

--- blog/urls.py
+++ blog/urls.py
@@ -4,9 +4,12 @@ from . import views
 urlpatterns = [
 	path('', views.dashboard, name='dashboard'),
 	path('post/', views.post_list, name='post_list'),
+	path('post/drafts', views.post_draft_list, name='post_draft_list'),
 	path('post/<int:pk>/', views.post_detail, name='post_detail'),
 	path('post/new/', views.post_new, name='post_new'),
 	path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+	path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
+	path('post/<int:pk>/remove/', views.post_remove, name='post_remove'),
 	path('jurnal/', views.jurnal_list, name='jurnal_list'),
 	path('jurnal/new/', views.jurnal_new, name='jurnal_new'),
 	path('jurnal/<int:pk>/edit/', views.jurnal_edit, name='jurnal_edit'),
--- blog/views.py
+++ blog/views.py
@@ -1,38 +1,45 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post, Jurnal
 from .forms import PostForm, JurnalForm
 # Create your views here.
+@login_required
 def dashboard(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	jurnals = Jurnal.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
 	return render(request, 'blog/dashboard.html', {'posts' : posts, 'jurnals' : jurnals})
 
+@login_required
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'blog/post_list.html', {'posts' : posts})
 
+@login_required
 def jurnal_list(request):
  	jurnals = Jurnal.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
  	return render(request,'blog/jurnal_list.html', {'jurnals' : jurnals})
 
+@login_required
 def post_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	return render(request, 'blog/post_detail.html', {'post' : post})
 
+@login_required
 def post_new(request):
 	if request.method == "POST":
 		form = PostForm(request.POST)
 		if form.is_valid():
 			post = form.save(commit=False)
 			post.author = request.user
-			post.published_date = timezone.now()
+			# post.published_date = timezone.now()
 			post.save()
 			return redirect('post_detail', pk=post.pk)
 	else:
 		form = PostForm()
 	return render(request, 'blog/post_edit.html', {'form' : form})
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -40,13 +47,31 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html',{'posts':posts})
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list');
+
+@login_required
 def jurnal_new(request):
  	if request.method == "POST":
  		form = JurnalForm(request.POST)
@@ -60,6 +85,7 @@ def jurnal_new(request):
  		form = JurnalForm()
  		return render(request, 'blog/jurnal_edit.html', {'form' : form})
 
+@login_required
 def jurnal_edit(request, pk):
     jurnal = get_object_or_404(Jurnal, pk=pk)
     if request.method == "POST":
